8b899329f8d28d237416029c8a15e5a2e28b09ee
rohitgupta7314 <91873370+rohitgupta7314@users.noreply.github.com>
2021-10-05 11:04:40 +0530

    Add files via upload

---
+++ Area of Circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int r;
+float PI=3.14 , area;
+printf("Enter the radius of circle");
+scanf("%d",&r);
+area= PI*r*r;
+printf("Area of Circle: %f", area);
+return 0;
+}
